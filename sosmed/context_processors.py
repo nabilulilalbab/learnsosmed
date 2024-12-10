e809199ddfad218754d7c121c722125d6702c6ad
nabilulilalbab <nabilulilalbab78@gmail.com>
2024-12-11 00:36:28 +0700

    add comment and category dinamis

--- sosmed/context_processors.py
+++ sosmed/context_processors.py
@@ -1,7 +1,12 @@
 from .models import Category
+from django.core.cache import cache
 
 
 
 
 def categories(request):
-    return {'categories': Category.objects.all()}
+    categories = cache.get('categories')
+    if not categories:
+        categories = Category.objects.all()
+        cache.set('categories', categories, 60 * 15)  # Cache selama 15 menit
+    return {'categories': categories}
